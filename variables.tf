354609b5a265601e47453111c1f31c753fcfaae7
algajallow@gmail.com <algajallow@gmail.com>
2025-04-11 06:35:23 -0500

    EKS cluster terraform codes

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "vpc_cidr_block" {
+  type = string
+}
+variable "subnet_cidr_block" {
+  type = list(string)
+}
+variable "availability_zone" {
+  type = list(string)
+}
---
+++ vpc/variables.tf
@@ -0,0 +1,12 @@
+variable "vpc_cidr_block" {
+  type = string
+}
+variable "tags" {
+  type = map(string)
+}
+variable "subnet_cidr_block" {
+  type = list(string)
+}
+variable "availability_zone" {
+  type = list(string)
+}
